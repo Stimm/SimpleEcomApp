152fcebc67a8ec2272fe7081a089cc3e8e81a01c
Stimm <timothyryan.dublin@gmail.com>
2025-04-04 17:42:36 +0100

    Created the new Items controler class.

---
+++ SimpleEcom/DataModels/Item.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace SimpleEcom.DataModels
+{
+    [DynamoDBTable("Items")]
+    public class Item
+    {
+        [DynamoDBHashKey("ItemId")]
+        public required string ItemId { get; set; }
+        public required string Name { get; set; }
+        public int Ammount { get; set; }
+        public string? Type { get; set; }
+        public string? Description { get; set; }
+        public string? ImageLarge { get; set; }
+        public string? ImageSmall { get; set; }
+
+    }
+}
